15bf387ed4ec9c5f30480149de8ecc144ba76c03
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-04-04 20:43:49 +0530

    Update header

--- src/app/components/IconGrid/index.tsx
+++ src/app/components/IconGrid/index.tsx
@@ -47,44 +47,44 @@ const heroBanner = [
 const IconGrid = () => {
   return (
     <>
-      <div className="w-full h-screen absolute flex items-center justify-center z-10">
-        <div className="flex space-x-80">
-          <div className="w-25% mx-auto  flex-wrap opacity-80 bg-slate-400 rounded">
-            <div className="flex flex-wrap">
-              <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-10 p-5">
-                {icons.map((icon, index) => (
-                  <div key={index} className="col-span-1 md:col-span-1 flex gap-5">
-                    <Card className={cn("w-[80px] h-[80px] card-hover-effects")}>
-                      <CardContent className="grid gap-3">
-                        <div className="rounded-md p-2">
-                          {/* <Image src={icon.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </div>
-                ))}
+      {/* <div className="w-full h-screen absolute flex items-center justify-center z-10"> */}
+      {/* <div className="space-x-80"> */}
+      <div className="w-25%  h-auto left-20 absolute mt-[18%] z-10 flex-wrap opacity-60 bg-black rounded">
+        <div className="flex flex-wrap">
+          <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-10 p-5">
+            {icons.map((icon, index) => (
+              <div key={index} className="col-span-1 md:col-span-1 flex gap-5">
+                <Card className={cn("w-[120px] h-[120px] card-hover-effects")}>
+                  <CardContent className="grid gap-3">
+                    <div className="rounded-md p-2">
+                      {/* <Image src={icon.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
+                    </div>
+                  </CardContent>
+                </Card>
               </div>
-            </div>
-          </div>
-
-          <div className="w-25% mx-auto  flex-wrap right-20 opacity-80 rounded">
-            <div className="flex ">
-              <div className="grid grid-cols-1 md:grid-cols-1">
-                {heroBanner.map((banner, index) => (
-                  <div key={index} className="col-span-1 md:col-span-1">
-                    <Card className={cn("w-[450px] h-[250px] card-hover-effects")}>
-                      <CardContent className="grid gap-3">
-                        <div className="rounded-xl p-2">
-                          {/* <Image src={banner.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </div>
-                ))}
-              </div>
-            </div>
+            ))}
           </div>
         </div>
+      </div>
+
+      <div className="w-25% h-auto absolute z-10 mt-[16%] float-right flex-wrap right-20 opacity-80 rounded">
+        <div className="flex ">
+          <div className="grid grid-cols-1 md:grid-cols-1">
+            {heroBanner.map((banner, index) => (
+              <div key={index} className="col-span-1 md:col-span-1">
+                <Card className={cn("w-[650px] h-[370px] card-hover-effects")}>
+                  <CardContent className="grid gap-3">
+                    <div className="rounded-xl p-2">
+                      {/* <Image src={banner.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* </div> */}
+        {/* </div> */}
       </div>
     </>
   );
