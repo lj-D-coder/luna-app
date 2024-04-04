4b0a1372edf416424c7488f025ea4e1434973a5a
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-04-05 00:07:00 +0530

    Header changes

---
+++ src/app/assets/images/index.ts
@@ -0,0 +1,20 @@
+import check from "./check.svg";
+import brainwaveSymbol from "./brainwave-symbol.svg";
+import carwash from "./car.svg";
+import carservice from "./car_service.svg"
+import accleaning from './ac_cleaning.svg'
+import homecleaning from './home_cleaning.svg'
+import sofaCleaning from './sofa_cleaning.svg'
+import phoneRepair from './phone_repair.svg'
+import sidebanner from './sidebanner.png'
+
+
+export {
+    carwash,
+    carservice,
+    accleaning,
+    homecleaning,
+    sofaCleaning,
+    phoneRepair,
+    sidebanner
+};
--- src/app/components/IconGrid/index.tsx
+++ src/app/components/IconGrid/index.tsx
@@ -1,38 +1,41 @@
 import React from "react";
 import { cn } from "@/lib/utils/cn";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/app/components/ui/card";
+import { carwash, accleaning, carservice, homecleaning, phoneRepair, sofaCleaning, sidebanner } from "@/app/assets/images";
 import Image from "next/image";
+import { url } from "inspector";
+
 
 const icons = [
   {
-    title: "Top rated professionals",
+    title: "Car Wash",
     description: "Our professionals are reliable & well-trained, with an average rating of 4.78 out of 5!",
-    url: "",
+    url: carwash
   },
   {
-    title: "Same-day availability",
+    title: "Car Servicing",
     description: "Book in less than 60 seconds, and even select same-day slots.",
-    url: "",
+    url: carservice
   },
   {
-    title: "value for money services",
+    title: "Ac Cleaning",
     description: "Our professionals are equipped with the best tools and our services .",
-    url: "",
+    url: accleaning
   },
   {
-    title: "Super app.",
+    title: "Home Cleaning",
     description: "Being a Super app means we’ve got the widest range of home services, so we’ve got you covered!",
-    url: "",
+    url: homecleaning
   },
   {
-    title: "value for money services",
+    title: "Sofa Cleaning",
     description: "Our professionals are equipped with the best tools and our services .",
-    url: "",
+    url: sofaCleaning
   },
   {
-    title: "Super app.",
+    title: "Phone Repair",
     description: "Being a Super app means we’ve got the widest range of home services, so we’ve got you covered!",
-    url: "",
+    url: phoneRepair
   },
 ];
 
@@ -40,7 +43,7 @@ const heroBanner = [
   {
     title: "Super app.",
     description: "Being a Super app means we’ve got the widest range of home services, so we’ve got you covered!",
-    url: "",
+    url: sidebanner,
   },
 ];
 
@@ -49,36 +52,32 @@ const IconGrid = () => {
     <>
       {/* <div className="w-full h-screen absolute flex items-center justify-center z-10"> */}
       {/* <div className="space-x-80"> */}
-      <div className="w-25%  h-auto left-20 absolute mt-[18%] z-10 flex-wrap opacity-60 bg-black rounded">
+      <div className="w-30% h-auto left-20 absolute mt-[15%] z-10 flex-wrap bg-white opacity-90 rounded">
         <div className="flex flex-wrap">
-          <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-10 p-5">
+          <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-10 p-10">
             {icons.map((icon, index) => (
-              <div key={index} className="col-span-1 md:col-span-1 flex gap-5">
-                <Card className={cn("w-[120px] h-[120px] card-hover-effects")}>
-                  <CardContent className="grid gap-3">
-                    <div className="rounded-md p-2">
-                      {/* <Image src={icon.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
-                    </div>
-                  </CardContent>
-                </Card>
+              <div key={index} className="col-span-1 md:col-span-1 flex-col">
+                <div className={cn("w-[100px] h-[100px] card-hover-effects border-none shadow-none")}>
+                  <Image
+                    src={icon.url}
+                    alt="car wash"
+                    layout="fill"
+                  />
+                </div>
+                <div className="text-center mt-2">{icon.title}</div>
               </div>
             ))}
           </div>
         </div>
       </div>
-
-      <div className="w-25% h-auto absolute z-10 mt-[16%] float-right flex-wrap right-20 opacity-80 rounded">
+      <div className="w-25% h-auto absolute z-10 mt-[13%] float-right flex-wrap right-20 opacity-80 rounded">
         <div className="flex ">
           <div className="grid grid-cols-1 md:grid-cols-1">
             {heroBanner.map((banner, index) => (
               <div key={index} className="col-span-1 md:col-span-1">
-                <Card className={cn("w-[650px] h-[370px] card-hover-effects")}>
-                  <CardContent className="grid gap-3">
-                    <div className="rounded-xl p-2">
-                      {/* <Image src={banner.url} alt="jordans" height="100" width="100" className="object-contain" /> */}
-                    </div>
-                  </CardContent>
-                </Card>
+                <div className={cn("w-[700px] h-[450px] card-hover-effects")}>
+                  <Image src={banner.url} alt="jordans" layout="fill" className="object-cover rounded-xl" />
+                </div>
               </div>
             ))}
           </div>
