aa64aef6ca321510c0e08b26b0f9650bdf76f31f
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-04-05 22:23:22 +0530

    home cleanup

--- src/app/components/IconGrid/index.tsx
+++ src/app/components/IconGrid/index.tsx
@@ -1,44 +1,53 @@
 import React from "react";
 import { cn } from "@/lib/utils/cn";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/app/components/ui/card";
-import { carwash, accleaning, carservice, homecleaning, phoneRepair, sofaCleaning, sidebanner, carwashimg, sofacleaningimg } from "@/app/assets/images";
+
+import {
+  carwash,
+  accleaning,
+  carservice,
+  homecleaning,
+  phoneRepair,
+  sofaCleaning,
+  sidebanner,
+  carwashimg,
+  sofacleaningimg,
+} from "@/app/assets/images";
 import Image from "next/image";
 import { url } from "inspector";
 import HeroSlider from "hero-slider/dist/HeroSlider";
 import Hero from "../Hero";
 import Search from "../Search";
 
-
 const icons = [
   {
     title: "Car Wash",
     description: "Our professionals are reliable & well-trained, with an average rating of 4.78 out of 5!",
-    url: carwash
+    url: carwash,
   },
   {
     title: "Car Servicing",
     description: "Book in less than 60 seconds, and even select same-day slots.",
-    url: carservice
+    url: carservice,
   },
   {
     title: "Ac Cleaning",
     description: "Our professionals are equipped with the best tools and our services .",
-    url: accleaning
+    url: accleaning,
   },
   {
     title: "Home Cleaning",
     description: "Being a Super app means we’ve got the widest range of home services, so we’ve got you covered!",
-    url: homecleaning
+    url: homecleaning,
   },
   {
     title: "Sofa Cleaning",
     description: "Our professionals are equipped with the best tools and our services .",
-    url: sofaCleaning
+    url: sofaCleaning,
   },
   {
     title: "Phone Repair",
     description: "Being a Super app means we’ve got the widest range of home services, so we’ve got you covered!",
-    url: phoneRepair
+    url: phoneRepair,
   },
 ];
 
@@ -53,44 +62,35 @@ const heroBanner = [
 const IconGrid = () => {
   return (
     <>
+    <div className="w-full h-screen grid grid-cols-1">
       <Search placeholder="Search for area, street name, landmark..." />
-      <div className="w-full h-screen items-center flex z-10 bg-white">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 space-x-5">
-          <div className="w-[450px] h-[300px] left-20 ml-[15%] mt-[25%]">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto"> {/* Added gap-4 for spacing */}
-              {icons.map((icon, index) => (
-                <div key={index} className="col-span-1 md:col-span-1 flex flex-col items-center hover:text-blue-400"> {/* Added items-center */}
-                  <div className={cn("w-[100px] h-[100px] card-hover-effects border-none shadow-none")}>
-                    <Image
-                      src={icon.url}
-                      alt="car wash"
-                      layout="fill"
-                    />
-                  </div>
-                  <div className="text-center mt-2">{icon.title}</div> {/* Added text-center class */}
+      <div className="h-full flex flex-row items-center bg-white justify-between mt-20 px-20">
+        <div className="w-[450px] h-[300px] self-center">
+          <div className="grid grid-cols-3 md:grid-cols-3 gap-4 mx-auto">
+            {icons.map((icon, index) => (
+              <div key={index} className="col-span-1 md:col-span-1 flex flex-col items-center hover:text-blue-400">
+                <div className={cn("w-[100px] h-[100px] card-hover-effects border-none shadow-none")}>
+                  <Image src={icon.url} alt="car wash" layout="fill" />
                 </div>
-              ))}
-            </div>
-          </div>
-
-
-          <div className="mt-[13%] rounded">
-            <div className="flex ">
-              <div className="grid grid-cols-1 md:grid-cols-1">
-                {heroBanner.map((banner, index) => (
-                  <div key={index} className="col-span-1 md:col-span-1">
-                    <div className={cn("w-[700px] h-[400px] card-hover-effects")}>
-                      <Hero />
-                      {/* <Image src={banner.url} alt="jordans" layout="fill" className="object-cover rounded-xl" /> */}
-                    </div>
-                  </div>
-                ))}
+                <div className="text-center mt-2">{icon.title}</div> {/* Added text-center class */}
               </div>
-            </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="w-[700px] card-hover-effects border-none shadow-none">
+            <Hero />
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
+  
+  
+
+  
+
+
   );
 };
 
