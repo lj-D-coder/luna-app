9512729dd9af72608cef45795d3e6f774557342b
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-04-13 17:15:14 +0530

    category routes

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,15 @@
+import Category from "@/app/(models)/Category";
+import { NextResponse } from "next/server";
+import connection from "@/lib/utils/db-connect";
+
+export async function GET(req: any): Promise<NextResponse> {
+    await connection();
+    try {
+        const categoryItem = await Category.find();
+        return NextResponse.json({ categoryItem, message: "Category Fetched" }, { status: 200 },);
+    } catch (error: any) {
+        console.error("Error caught", error);
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+}
+
