ed0a8fcfc3ffcdd3f269bcca01f56765f8a7322d
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-04-23 14:49:07 +0530

    Service with Caps

---
+++ src/components/ServiceBooking/Service.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { FC, SetStateAction, useEffect, useState } from "react";
+import { SubCategory } from "./SubCategoryGrid";
+import { ServicesGrid } from "./ServicesGrid";
+
+interface ServiceBookingProps {
+  categoryUrl: string;
+}
+
+export const Services: FC<ServiceBookingProps> = ({ categoryUrl }) => {
+  const [selectedSubcategory, setSelectedSubcategory] = useState("");
+
+  const handleIconClick = (subCategory: string) => {
+    console.log(subCategory);
+    setSelectedSubcategory(subCategory);
+  };
+
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-12 md:col-span-4">
+        <SubCategory onIconClick={handleIconClick} categoryUrl={categoryUrl} />
+      </div>
+      <div className="col-span-12 md:col-span-8">
+        <ServicesGrid selectedSubcategory={selectedSubcategory} categoryUrl={categoryUrl} />
+      </div>
+    </div>
+  );
+};
